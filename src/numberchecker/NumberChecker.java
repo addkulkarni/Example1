90825ee0102228b89067af9be6b8214a5463c83a
addku <addku@MyPC>
2024-06-19 00:49:53 +0530

    First commit

---
+++ src/numberchecker/NumberChecker.java
@@ -0,0 +1,30 @@
+package numberchecker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberChecker
+{
+	public static void main(String[] args)
+	{
+		String s="10.Aditya 2.asd_2202 3.rty";
+		
+		List<String>l = new ArrayList<>();
+		for(int i=0; i<s.length(); i++)
+		{
+			if(Character.isDigit(s.charAt(i)))
+			{
+				int j=i;
+				System.out.println(s.charAt(i));
+				String s1="";
+				while(Character.isDigit(s.charAt(j++)))
+				{
+					s1=s1+s.charAt(i);
+					System.out.println("f");
+				}
+				l.add(s1);
+			}
+		}
+		System.out.println(l);
+	}
+}
